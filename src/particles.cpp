d32b85c7434c1192d3b430342de54e5d043f1825
NodmGatall <oliverheidmann@hotmail.de>
2016-10-27 19:49:45 +0200

    foramted particles

--- src/particles.cpp
+++ src/particles.cpp
@@ -3,12 +3,11 @@
 #include <glm/glm.hpp>
 #include <vector>
 
-glm::vec3           Particles::get_velocity(int particle_id) {
+glm::vec3 Particles::get_velocity(int particle_id) {
 
-    
-                return m_velocities[particle_id];
+    return m_velocities[particle_id];
 }
 
-std::vector<glm::vec3>       *Particles::get_velocities
-() { 
-    return &m_velocities;        }
+std::vector<glm::vec3> *Particles::get_velocities() {
+    return &m_velocities;
+}
