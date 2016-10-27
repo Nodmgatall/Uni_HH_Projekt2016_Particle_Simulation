70dab4b62eedfb2ea73ae766e9398cfd20d11e69
NodmGatall <oliverheidmann@hotmail.de>
2016-10-27 19:48:52 +0200

    added clang format

--- src/particles.cpp
+++ src/particles.cpp
@@ -1,12 +1,14 @@
 #include "particles.hpp"
 
-#include <vector>
 #include <glm/glm.hpp>
+#include <vector>
 
-glm::vec3 Particles::get_velocity(int particle_id) {
-    return m_velocities[particle_id];
+glm::vec3           Particles::get_velocity(int particle_id) {
+
+    
+                return m_velocities[particle_id];
 }
 
-std::vector<glm::vec3> *Particles::get_velocities() {
-    return &m_velocities;
-}
+std::vector<glm::vec3>       *Particles::get_velocities
+() { 
+    return &m_velocities;        }
