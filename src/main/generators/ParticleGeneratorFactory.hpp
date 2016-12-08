b8ce70e26d2914c082a20d08a93b2cb4d6c5407c
benjamin <benjamin@benjamin-desktop-ubuntu>
2016-12-08 23:06:20 +0100

    added test for exceptions

--- src/main/generators/ParticleGeneratorFactory.hpp
+++ src/main/generators/ParticleGeneratorFactory.hpp
@@ -15,40 +15,45 @@
 #include "ParticleGeneratorGridDistribution.hpp"
 #include "ParticleGeneratorRandom.hpp"
 #include "ParticleGeneratorSingleObjectMiddle.hpp"
-
+#include <cstdio>
 #include <memory>
 #include <unistd.h>
-class GeneratorInvalidException : public std::exception {
-    private:
-    int m_generator_mode;
+class GeneratorInvalidException: public std::exception {
+private:
+	int m_generator_mode;
 
-    public:
-    GeneratorInvalidException () : m_generator_mode (-1) {
-    }
-    GeneratorInvalidException (int p_generator_mode) : m_generator_mode (p_generator_mode) {
-    }
-    virtual const char *what () const throw () {
-        return (std::string ("generator mode ( ") + std::to_string (m_generator_mode) +
-                " ) is undefined")
-            .c_str ();
-    }
+public:
+	GeneratorInvalidException() :
+			m_generator_mode(-1) {
+	}
+	GeneratorInvalidException(int p_generator_mode) :
+			m_generator_mode(p_generator_mode) {
+	}
+	virtual const char *what() const throw () {
+		char *text = (char*) malloc(100);
+		sprintf(text, "generator mode ( %d ) is undefined",
+				m_generator_mode);
+		return text;
+	}
 };
-class GeneratorNotImplementedException : public std::exception {
-    private:
-    int m_generator_mode;
+class GeneratorNotImplementedException: public std::exception {
+private:
+	int m_generator_mode;
 
-    public:
-    GeneratorNotImplementedException (int p_generator_mode) : m_generator_mode (p_generator_mode) {
-    }
-    virtual const char *what () const throw () {
-        return (std::string ("generator mode ( ") + std::to_string (m_generator_mode) +
-                " ) is not (fully) implemented")
-            .c_str ();
-    }
+public:
+	GeneratorNotImplementedException(int p_generator_mode) :
+			m_generator_mode(p_generator_mode) {
+	}
+	virtual const char *what() const throw () {
+		char *text = (char*) malloc(100);
+		sprintf(text, "generator mode ( %d ) is not (fully) implemented",
+				m_generator_mode);
+		return text;
+	}
 };
 class ParticleGeneratorFactory {
-    public:
-    static std::unique_ptr<ParticleGeneratorBase> build (s_options &p_options);
+public:
+	static std::unique_ptr<ParticleGeneratorBase> build(s_options &p_options);
 };
 
 #endif /* SRC_PARTICLEGENERATOR_HPP_ */
--- src/test/generators/test_ParticleGeneratorFactory.cpp
+++ src/test/generators/test_ParticleGeneratorFactory.cpp
@@ -12,41 +12,69 @@
 #include <cstring>
 
 BOOST_AUTO_TEST_CASE (testParticleGeneratorRandom) {
-    s_options options;
-    memset (&options, 0, sizeof (s_options));
-    options.m_mode                                   = e_generator_mode::RANDOM;
-    std::unique_ptr<ParticleGeneratorBase> generator = 0;
-    BOOST_CHECK_NO_THROW (generator = ParticleGeneratorFactory::build (options));
-    BOOST_CHECK_EQUAL (generator->get_stucture_name (), "ParticleGeneratorRandom");
+	s_options options;
+	memset(&options, 0, sizeof(s_options));
+	options.m_mode = e_generator_mode::RANDOM;
+	std::unique_ptr<ParticleGeneratorBase> generator = 0;
+	BOOST_CHECK_NO_THROW(generator = ParticleGeneratorFactory::build(options));
+	BOOST_CHECK_EQUAL(generator->get_stucture_name(),
+			"ParticleGeneratorRandom");
 }
 BOOST_AUTO_TEST_CASE (testParticleGeneratorSingleObjectMiddle) {
-    s_options options;
-    memset (&options, 0, sizeof (s_options));
-    options.m_mode                                   = e_generator_mode::SINGLE_OBJECT_MIDDLE;
-    std::unique_ptr<ParticleGeneratorBase> generator = 0;
-    BOOST_CHECK_NO_THROW (generator = ParticleGeneratorFactory::build (options));
-    BOOST_CHECK_EQUAL (generator->get_stucture_name (), "ParticleGeneratorSingleObjectMiddle");
+	s_options options;
+	memset(&options, 0, sizeof(s_options));
+	options.m_mode = e_generator_mode::SINGLE_OBJECT_MIDDLE;
+	std::unique_ptr<ParticleGeneratorBase> generator = 0;
+	BOOST_CHECK_NO_THROW(generator = ParticleGeneratorFactory::build(options));
+	BOOST_CHECK_EQUAL(generator->get_stucture_name(),
+			"ParticleGeneratorSingleObjectMiddle");
 }
 BOOST_AUTO_TEST_CASE (testParticleGeneratorGridDistribution) {
-    s_options options;
-    memset (&options, 0, sizeof (s_options));
-    options.m_mode                                   = e_generator_mode::GRID_DISTRIBUTION;
-    std::unique_ptr<ParticleGeneratorBase> generator = 0;
-    BOOST_CHECK_NO_THROW (generator = ParticleGeneratorFactory::build (options));
-    BOOST_CHECK_EQUAL (generator->get_stucture_name (), "ParticleGeneratorGridDistribution");
+	s_options options;
+	memset(&options, 0, sizeof(s_options));
+	options.m_mode = e_generator_mode::GRID_DISTRIBUTION;
+	std::unique_ptr<ParticleGeneratorBase> generator = 0;
+	BOOST_CHECK_NO_THROW(generator = ParticleGeneratorFactory::build(options));
+	BOOST_CHECK_EQUAL(generator->get_stucture_name(),
+			"ParticleGeneratorGridDistribution");
 }
 
 BOOST_AUTO_TEST_CASE (testParticleGeneratorMulipleObjects) {
-    s_options options;
-    memset (&options, 0, sizeof (s_options));
-    options.m_mode                                   = e_generator_mode::MULTIPLE_OBJECTS;
-    std::unique_ptr<ParticleGeneratorBase> generator = 0;
-    BOOST_CHECK_THROW (generator = ParticleGeneratorFactory::build (options), GeneratorNotImplementedException);
+	s_options options;
+	memset(&options, 0, sizeof(s_options));
+	options.m_mode = e_generator_mode::MULTIPLE_OBJECTS;
+	std::unique_ptr<ParticleGeneratorBase> generator = 0;
+	BOOST_CHECK_THROW(generator = ParticleGeneratorFactory::build(options),
+			GeneratorNotImplementedException);
 }
 BOOST_AUTO_TEST_CASE (testParticleGeneratorRandomUniform) {
-    s_options options;
-    memset (&options, 0, sizeof (s_options));
-    options.m_mode                                   = e_generator_mode::RANDOM_UNIFORM;
-    std::unique_ptr<ParticleGeneratorBase> generator = 0;
-    BOOST_CHECK_THROW (generator = ParticleGeneratorFactory::build (options), GeneratorNotImplementedException);
+	s_options options;
+	memset(&options, 0, sizeof(s_options));
+	options.m_mode = e_generator_mode::RANDOM_UNIFORM;
+	std::unique_ptr<ParticleGeneratorBase> generator = 0;
+	BOOST_CHECK_THROW(generator = ParticleGeneratorFactory::build(options),
+			GeneratorNotImplementedException);
 }
+BOOST_AUTO_TEST_CASE (testParticleGeneratorError) {
+	s_options options;
+	memset(&options, 0, sizeof(s_options));
+	options.m_mode = static_cast<e_generator_mode>(-1);
+	std::unique_ptr<ParticleGeneratorBase> generator = 0;
+	BOOST_CHECK_THROW(generator = ParticleGeneratorFactory::build(options),
+			GeneratorInvalidException);
+}
+BOOST_AUTO_TEST_CASE (testGeneratorNotImplementedException) {
+	GeneratorNotImplementedException exception(
+			e_generator_mode::GRID_DISTRIBUTION);
+	BOOST_CHECK_EQUAL(std::string(exception.what()),
+			"generator mode ( 4 ) is not (fully) implemented");
+}
+BOOST_AUTO_TEST_CASE (testGeneratorInvalidException) {
+	GeneratorInvalidException exception(-1);
+	BOOST_CHECK_EQUAL(std::string(exception.what()),
+			"generator mode ( -1 ) is undefined");
+}
+
+
+
+
