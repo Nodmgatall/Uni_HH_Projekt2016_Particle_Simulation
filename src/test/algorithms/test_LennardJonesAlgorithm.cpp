fb38b749ed26ffa05596a2df3b9d844c48b35262
benjamin <benjamin@benjamin-desktop-ubuntu>
2016-12-08 17:47:26 +0100

    completed lennard-jones tests

--- src/test/algorithms/test_LennardJonesAlgorithm.cpp
+++ src/test/algorithms/test_LennardJonesAlgorithm.cpp
@@ -21,3 +21,33 @@ BOOST_AUTO_TEST_CASE (step1) {
     for (int i = 0; i < 3; i++)
         BOOST_CHECK_EQUAL (v[3 + i], i - 2);
 }
+BOOST_AUTO_TEST_CASE (step2_1) {
+    s_simulator_options options;
+    memset (&options, 0, sizeof (s_simulator_options));
+    options.m_timestep = 1;
+    LennardJonesAlgorithm::init (options);
+    data_type v[] = { 1, 2, 3, 2, 2, 3, 1, 2, 3, 2, 2, 3 };
+    LennardJonesAlgorithm::step_2 (v[0], v[1], v[2], v[6], v[7], v[8], v[3], v[4], v[5], v[9], v[10], v[11]);
+    BOOST_CHECK_EQUAL (v[6] - v[0], 24);
+    BOOST_CHECK_EQUAL (v[9] - v[3], -24);
+}
+BOOST_AUTO_TEST_CASE (step2_2) {
+    s_simulator_options options;
+    memset (&options, 0, sizeof (s_simulator_options));
+    options.m_timestep = 1;
+    LennardJonesAlgorithm::init (options);
+    data_type v[] = { 1, 2, 3, 1, 3, 3, 1, 2, 3, 1, 3, 3 };
+    LennardJonesAlgorithm::step_2 (v[0], v[1], v[2], v[6], v[7], v[8], v[3], v[4], v[5], v[9], v[10], v[11]);
+    BOOST_CHECK_EQUAL (v[7] - v[1], 24);
+    BOOST_CHECK_EQUAL (v[10] - v[4], -24);
+}
+BOOST_AUTO_TEST_CASE (step2_3) {
+    s_simulator_options options;
+    memset (&options, 0, sizeof (s_simulator_options));
+    options.m_timestep = 1;
+    LennardJonesAlgorithm::init (options);
+    data_type v[] = { 1, 2, 3, 1, 2, 4, 1, 2, 3, 1, 2, 4 };
+    LennardJonesAlgorithm::step_2 (v[0], v[1], v[2], v[6], v[7], v[8], v[3], v[4], v[5], v[9], v[10], v[11]);
+    BOOST_CHECK_EQUAL (v[8] - v[2], 24);
+    BOOST_CHECK_EQUAL (v[11] - v[5], -24);
+}
