19f49ae6392b3fb6bad6e28296e28adf3346968d
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2016-12-08 10:10:30 +0100

    begin implementation of particlebordercorrection test

--- src/test/algorithms/test_LennardJonesAlgorithm.cpp
+++ src/test/algorithms/test_LennardJonesAlgorithm.cpp
@@ -5,7 +5,7 @@
  *      Author: benjamin
  */
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE "Vec3f"
+#define BOOST_TEST_MODULE "LennardJonesAlgorithm"
 #include "../../main/algorithms/LennardJonesAlgorithm.hpp"
 #include <boost/test/unit_test.hpp>
 #include <cstring>
@@ -20,18 +20,4 @@ BOOST_AUTO_TEST_CASE (step1) {
         BOOST_CHECK_EQUAL (v[i], 1 + i);
     for (int i = 0; i < 3; i++)
         BOOST_CHECK_EQUAL (v[3 + i], i - 2);
-}
-BOOST_AUTO_TEST_CASE (step2) {
-    s_simulator_options options;
-    memset (&options, 0, sizeof (s_simulator_options));
-    options.m_timestep = 1;
-    LennardJonesAlgorithm::init (options);
-    data_type v[] = { 1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6 };
-    LennardJonesAlgorithm::step_2 (v[0], v[1], v[2], v[6], v[7], v[8], v[3], v[4], v[5], v[9], v[10], v[11]);
-    for (int i = 0; i < 6; i++) {
-        BOOST_CHECK_EQUAL (v[i], 1 + i);
-    }
-    for (int i = 0; i < 6; i++) {
-        BOOST_CHECK_CLOSE (v[6 + i], i + 1, 0.1); // TODO this check is useless!!
-    }
 }
---
+++ src/test/borders/test_ParticleBoundsCorrectionWraparound.cpp
@@ -0,0 +1,19 @@
+/*
+ * test_ParticleBoundsCorrectionWraparound.cpp
+ *
+ *  Created on: 08.12.2016
+ *      Author: benjamin
+ */
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "ParticleBoundsCorrectionWraparound"
+#include "../../main/borders/ParticleBoundsCorrectionWraparound.hpp"
+#include "../../main/Vec3.hpp"
+#include <boost/test/unit_test.hpp>
+BOOST_AUTO_TEST_CASE (test1) {
+	Vec3f bounds(3,4,5);
+	ParticleBoundsCorrectionWraparound particleBoundsCorrectionWraparound(bounds);
+	data_type v[] = { 1, 2, 3, 101,102,103 };
+	particleBoundsCorrectionWraparound.updatePosition(v[0],v[1],v[2],v[3],v[4],v[5]);
+
+	//TODO !!!!!!!!!
+}
