42c8e440e5113d478bd00947b44da75b81496aff
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2016-12-08 08:12:42 +0100

    format

--- src/main/datastructures/ParticlesGrid.hpp
+++ src/main/datastructures/ParticlesGrid.hpp
@@ -40,6 +40,7 @@ class ParticlesGrid : public ParticlesBase {
     inline void step_2a_calculate_inside_cell (ParticleCell &p_cell);
     inline void step_2b_calculate_betweenCells (ParticleCell &p_cell1, ParticleCell &p_cell2);
     inline void step_3_remove_wrong_particles_from_cell (ParticleCell &p_cell);
+
     public:
     ParticlesGrid (s_simulator_options *p_options, Vec3f *p_bounds);
     ~ParticlesGrid ();
