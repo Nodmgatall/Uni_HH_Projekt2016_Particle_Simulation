cdbb3cb31859875a113aa6d3e01d19ffc66e607f
benjamin <benjamin@benjamin-desktop-ubuntu>
2016-12-09 18:52:03 +0100

    comments

--- src/main/datastructures/ParticlesGrid.hpp
+++ src/main/datastructures/ParticlesGrid.hpp
@@ -5,31 +5,54 @@
 #include "../Vec3.hpp"
 #include "ParticlesBase.hpp"
 
-/*
- * Partikel, die auf der einen Seite das Grid verlassen, kommen auf der anderen
- * Seite wieder rein.
- * */
 struct ParticleCell {
     std::vector<data_type>     m_positions_x[2];
     std::vector<data_type>     m_positions_y[2];
     std::vector<data_type>     m_positions_z[2];
     std::vector<unsigned long> m_ids;
     Vec3f                      m_corner000, m_corner111;
-    Vec3l                      m_idx; // bezug auf ParticlesGrid::m_cells
+	Vec3l m_idx; // refer to ParticlesGrid::m_cells
     ParticleCell (Vec3l p_idx, Vec3l p_size, Vec3f &p_bounds);
     void add_particle (Vec3f p_current_position, Vec3f p_old_position, int p_current_index, int p_id);
 };
 
 class ParticlesGrid : public ParticlesBase {
   protected:
+	/**
+	 * the index for the "next" inserted particle
+	 */
     unsigned long             m_max_id;
+	/**
+	 * the cells in which the particles are stored
+	 */
     std::vector<ParticleCell> m_cells;
+	/**
+	 * the count of cells in 3 dimensions
+	 * refer to m_cells
+	 */
     Vec3l                     m_size;
+	/**
+	 * the coordinate space captured by each cell
+	 */
     Vec3f                     m_size_per_cell;
+	/**
+	 * the number of iterations between to tests that all particles stay within the given bounds
+	 */
     unsigned const int        m_iterations_between_rearange_particles;
+	/**
+	 * the number of iterations until next test that all particles stay within the given bounds
+	 */
     unsigned int              m_iterations_until_rearange_particles;
-    unsigned int              m_idx_a; // aktueller index
-    unsigned int              m_idx_b; // neuer/alter index :: es gilt immer m_idx_a=!m_idx_b
+	/**
+	 * the current position of particles is stored in the array with this index
+	 * m_idx_a != m_idx_b
+	 */
+	unsigned int m_idx_a;
+	/**
+	 * the last/next position of particles is stored in the array with this index
+	 * m_idx_a != m_idx_b
+	 */
+	unsigned int m_idx_b;
     unsigned long get_cell_index (long x, long y, long z);
     ParticleCell &get_cell_at (long x, long y, long z);
     ParticleCell &get_cell_for_particle (data_type x, data_type y, data_type z);
