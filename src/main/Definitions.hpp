0dc1d472033fb3f5f4f61d30723c9398ba98687b
benjamin <benjamin@benjamin-desktop-ubuntu>
2016-12-10 21:37:44 +0100

    removed doubleincludes

--- src/main/Definitions.hpp
+++ src/main/Definitions.hpp
@@ -6,15 +6,23 @@
  */
 #ifndef DEFINITIONS_HPP_
 #define DEFINITIONS_HPP_
-#include "DataType.hpp"
+#ifndef MIN
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#endif
+#ifndef MAX
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#endif
 #ifdef OMP_AVAILABLE
 #include <omp.h>
 #endif
+#include "DataType.hpp"
 #include "enums/EnumAlgorithmType.hpp"
 #include "enums/EnumCSVColumnType.hpp"
 #include "enums/EnumDatastructureType.hpp"
 #include "enums/EnumInputType.hpp"
 #include "enums/EnumOutputType.hpp"
+#include "tools/Benchmark.hpp"
+#include "tools/Debug.hpp"
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
@@ -24,18 +32,11 @@
 #include <map>
 #include <math.h>
 #include <memory>
-#include <memory>
 #include <sstream>
 #include <string>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <unistd.h>
 #include <vector>
 
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-
-#include "tools/Benchmark.hpp"
-#include "tools/Debug.hpp"
 #endif /* DEFINITIONS_HPP_ */
