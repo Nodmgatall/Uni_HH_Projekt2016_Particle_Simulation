5749a697087f616fde6226d9fabcf036187a16db
benjamin <benjamin@benjamin-desktop-ubuntu>
2016-12-11 17:23:48 +0100

    renamed files to reuse them for testing

--- src/test/io/output/file/test_FileWriterCSV.cpp
+++ src/test/io/output/file/test_FileWriterCSV.cpp
@@ -10,13 +10,6 @@
 #include "io/output/file/FileWriterCSV.hpp"
 #include "string"
 #include <boost/test/unit_test.hpp>
-void compareFiles (std::string name) {
-    std::ifstream               ifs1 ("../Temporary/" + name);
-    std::ifstream               ifs2 ("../Resources/" + name);
-    std::istream_iterator<char> b1 (ifs1), e1;
-    std::istream_iterator<char> b2 (ifs2), e2;
-    BOOST_CHECK_EQUAL_COLLECTIONS (b1, e1, b2, e2);
-}
 
 BOOST_AUTO_TEST_CASE (test1) {
     std::map<e_csv_column_type, bool> write_modes = { { e_csv_column_type::ID, true },
@@ -27,7 +20,12 @@ BOOST_AUTO_TEST_CASE (test1) {
     FileWriterCSV writer (write_modes, "../Temporary/test_FileWriterCSV.test1.tmp.");
     writer.start ();
     writer.end ();
-    compareFiles ("test_FileWriterCSV.test1.tmp.0.csv");
+    //
+    std::ifstream               ifs1 ("../Temporary/test1.csv");
+    std::ifstream               ifs2 ("../Resources/test_FileWriterCSV.test1.tmp.0.csv");
+    std::istream_iterator<char> b1 (ifs1), e1;
+    std::istream_iterator<char> b2 (ifs2), e2;
+    BOOST_CHECK_EQUAL_COLLECTIONS (b1, e1, b2, e2);
 }
 BOOST_AUTO_TEST_CASE (test2) {
     std::map<e_csv_column_type, bool> write_modes = { { e_csv_column_type::ID, true },
@@ -49,5 +47,10 @@ BOOST_AUTO_TEST_CASE (test2) {
     }
     writer.saveData (positions_x, positions_y, positions_z, ids);
     writer.end ();
-    compareFiles ("test_FileWriterCSV.test2.tmp.0.csv");
+    //
+    std::ifstream               ifs1 ("../Temporary/test2.csv");
+    std::ifstream               ifs2 ("../Resources/test_FileWriterCSV.test2.tmp.0.csv");
+    std::istream_iterator<char> b1 (ifs1), e1;
+    std::istream_iterator<char> b2 (ifs2), e2;
+    BOOST_CHECK_EQUAL_COLLECTIONS (b1, e1, b2, e2);
 }
