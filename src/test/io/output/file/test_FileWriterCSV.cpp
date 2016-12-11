5c22a1352d2a439f24efafad8441c511df9c3f4f
benjamin <benjamin@benjamin-desktop-ubuntu>
2016-12-11 17:00:06 +0100

    added testing folder to git to store comparefiles there

--- src/test/io/output/file/test_FileWriterCSV.cpp
+++ src/test/io/output/file/test_FileWriterCSV.cpp
@@ -7,14 +7,16 @@
 
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE "FileWriterCSV"
-#include "io/output/WriterBase.hpp"
+#include "io/output/file/FileWriterCSV.hpp"
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE (test1) {
-    /* FileWriterCSV writer ({ { e_csv_column_type::ID, false },
-                             { e_csv_column_type::POSITION, false },
-                             { e_csv_column_type::VELOCITY, false },
-                             { e_csv_column_type::ACCELERATION, false },
-                             { e_csv_column_type::PARTICLE_TYPE, false } },
-                           "theFileName.testfile");*/
+    std::map<e_csv_column_type, bool> write_modes = { { e_csv_column_type::ID, true },
+                                                      { e_csv_column_type::POSITION, true },
+                                                      { e_csv_column_type::VELOCITY, false },
+                                                      { e_csv_column_type::ACCELERATION, false },
+                                                      { e_csv_column_type::PARTICLE_TYPE, false } };
+    FileWriterCSV writer (write_modes, "test_FileWriterCSV.test1.tmp.");
+    writer.start ();
+    writer.end ();
 }
