d5ae3757432703a1cea88f726a634052b6a13f66
benjamin <benjamin@benjamin-desktop-ubuntu>
2016-12-11 13:04:41 +0100

    add test

---
+++ src/test/enums/test_EnumInputType.cpp
@@ -0,0 +1,70 @@
+/*
+ * test_Vec3.cpp
+ *
+ *  Created on: 07.12.2016
+ *      Author: benjamin
+ */
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "EnumInputType"
+#include "../../main/enums/EnumInputType.hpp"
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+
+BOOST_AUTO_TEST_CASE (testException1) {
+    InputTypeInvalidException exception (e_input_type::RANDOM);
+    BOOST_CHECK_EQUAL (exception.what (), "algorithm type ( RANDOM ) is invalid");
+}
+BOOST_AUTO_TEST_CASE (testException2) {
+    InputTypeInvalidException exception (e_input_type::RANDOM_UNIFORM);
+    BOOST_CHECK_EQUAL (exception.what (), "algorithm type ( RANDOM_UNIFORM ) is invalid");
+}
+BOOST_AUTO_TEST_CASE (testException3) {
+    InputTypeInvalidException exception (e_input_type::SINGLE_OBJECT_MIDDLE);
+    BOOST_CHECK_EQUAL (exception.what (), "algorithm type ( SINGLE_OBJECT_MIDDLE ) is invalid");
+}
+BOOST_AUTO_TEST_CASE (testException4) {
+    InputTypeInvalidException exception (e_input_type::GRID_DISTRIBUTION);
+    BOOST_CHECK_EQUAL (exception.what (), "algorithm type ( GRID_DISTRIBUTION ) is invalid");
+}
+BOOST_AUTO_TEST_CASE (testException5) {
+    InputTypeInvalidException exception (e_input_type::MULTIPLE_OBJECTS);
+    BOOST_CHECK_EQUAL (exception.what (), "algorithm type ( MULTIPLE_OBJECTS ) is invalid");
+}
+BOOST_AUTO_TEST_CASE (testException6) {
+    InputTypeInvalidException exception (e_input_type::CSV);
+    BOOST_CHECK_EQUAL (exception.what (), "algorithm type ( CSV ) is invalid");
+}
+BOOST_AUTO_TEST_CASE (testString1) {
+    std::stringstream s;
+    s << e_input_type::RANDOM;
+    BOOST_CHECK_EQUAL (s.str (), "RANDOM");
+}
+BOOST_AUTO_TEST_CASE (testString2) {
+    std::stringstream s;
+    s << e_input_type::RANDOM_UNIFORM;
+    BOOST_CHECK_EQUAL (s.str (), "RANDOM_UNIFORM");
+}
+BOOST_AUTO_TEST_CASE (testString3) {
+    std::stringstream s;
+    s << e_input_type::SINGLE_OBJECT_MIDDLE;
+    BOOST_CHECK_EQUAL (s.str (), "SINGLE_OBJECT_MIDDLE");
+}
+BOOST_AUTO_TEST_CASE (testString4) {
+    std::stringstream s;
+    s << e_input_type::GRID_DISTRIBUTION;
+    BOOST_CHECK_EQUAL (s.str (), "GRID_DISTRIBUTION");
+}
+BOOST_AUTO_TEST_CASE (testString5) {
+    std::stringstream s;
+    s << e_input_type::MULTIPLE_OBJECTS;
+    BOOST_CHECK_EQUAL (s.str (), "MULTIPLE_OBJECTS");
+}
+BOOST_AUTO_TEST_CASE (testString6) {
+    std::stringstream s;
+    s << e_input_type::CSV;
+    BOOST_CHECK_EQUAL (s.str (), "CSV");
+}
+BOOST_AUTO_TEST_CASE (testStringException) {
+    std::stringstream s;
+    BOOST_CHECK_THROW (s << (static_cast<e_input_type> (0)), InputTypeInvalidException);
+}
