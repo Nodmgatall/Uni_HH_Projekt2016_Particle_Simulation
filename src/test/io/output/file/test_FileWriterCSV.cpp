010949c50895c1b8b0076fee676418c21ab3724a
benjamin <benjamin@benjamin-desktop-ubuntu>
2016-12-11 17:20:00 +0100

    tested fileoutput

--- src/test/io/output/file/test_FileWriterCSV.cpp
+++ src/test/io/output/file/test_FileWriterCSV.cpp
@@ -29,3 +29,25 @@ BOOST_AUTO_TEST_CASE (test1) {
     writer.end ();
     compareFiles ("test_FileWriterCSV.test1.tmp.0.csv");
 }
+BOOST_AUTO_TEST_CASE (test2) {
+    std::map<e_csv_column_type, bool> write_modes = { { e_csv_column_type::ID, true },
+                                                      { e_csv_column_type::POSITION, true },
+                                                      { e_csv_column_type::VELOCITY, false },
+                                                      { e_csv_column_type::ACCELERATION, false },
+                                                      { e_csv_column_type::PARTICLE_TYPE, false } };
+    FileWriterCSV writer (write_modes, "../Temporary/test_FileWriterCSV.test2.tmp.");
+    writer.start ();
+    std::vector<data_type>     positions_x;
+    std::vector<data_type>     positions_y;
+    std::vector<data_type>     positions_z;
+    std::vector<unsigned long> ids;
+    for (int i = 0; i < 40; i += 4) {
+        positions_x.push_back (i + 0.5);
+        positions_y.push_back (i + 1.5);
+        positions_z.push_back (i + 2.5);
+        ids.push_back (i + 3);
+    }
+    writer.saveData (positions_x, positions_y, positions_z, ids);
+    writer.end ();
+    compareFiles ("test_FileWriterCSV.test2.tmp.0.csv");
+}
