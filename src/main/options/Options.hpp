fdf61733dcb8c76eff43130863267c019468717f
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2016-12-14 19:03:15 +0100

    options now has an constructor

--- src/main/options/Options.hpp
+++ src/main/options/Options.hpp
@@ -5,24 +5,32 @@
 #include <map>
 #include <vector>
 struct s_options {
-    e_algorithm_type            m_algorithm_type      = e_algorithm_type::LENNARD_JONES;
-    bool                        m_autotuneing         = false;
-    Vec3f                       m_bounds              = Vec3f (5.0f, 5.0f, 5.0f);
-    data_type                   m_cut_off_radius      = 0.01;
-    e_datastructure_type        m_data_structure_type = e_datastructure_type::GRID;
-    e_input_type                m_input_type          = e_input_type::GENERATOR_GRID_DISTRIBUTION;
-    std::string                 m_in_file_name        = "";
-    unsigned long               m_max_iterations      = 0;
-    int                         m_max_iterations_between_datastructure_rebuild = 20;
-    e_output_type               m_output_type    = e_output_type::FILE_CSV;
-    std::string                 m_out_file_name  = "";
-    unsigned long               m_particle_count = 0;
-    unsigned int                m_seed           = 123456789;
-    data_type                   m_timestep       = 1;
-    bool                        m_verbose        = false;
-    int                         m_write_fequency = 1;
-    std::set<e_csv_column_type> m_write_modes    = { e_csv_column_type::ID,
-                                                  e_csv_column_type::POSITION,
-                                                  e_csv_column_type::VELOCITY };
+    e_algorithm_type            m_algorithm_type;
+    bool                        m_autotuneing;
+    Vec3f                       m_bounds;
+    data_type                   m_cut_off_radius;
+    e_datastructure_type        m_data_structure_type;
+    e_input_type                m_input_type;
+    std::string                 m_in_file_name;
+    unsigned long               m_max_iterations;
+    int                         m_max_iterations_between_datastructure_rebuild;
+    e_output_type               m_output_type;
+    std::string                 m_out_file_name;
+    unsigned long               m_particle_count;
+    unsigned int                m_seed;
+    data_type                   m_timestep;
+    bool                        m_verbose;
+    int                         m_write_fequency;
+    std::set<e_csv_column_type> m_write_modes;
+    s_options ()
+    : m_algorithm_type (e_algorithm_type::LENNARD_JONES), m_autotuneing (false),
+      m_bounds (Vec3f (5.0f, 5.0f, 5.0f)), m_cut_off_radius (0.01),
+      m_data_structure_type (e_datastructure_type::GRID),
+      m_input_type (e_input_type::GENERATOR_GRID_DISTRIBUTION), m_in_file_name (""),
+      m_max_iterations (0), m_max_iterations_between_datastructure_rebuild (20),
+      m_output_type (e_output_type::FILE_CSV), m_out_file_name (""), m_particle_count (0),
+      m_seed (123456789), m_timestep (1), m_verbose (false), m_write_fequency (1),
+      m_write_modes ({ e_csv_column_type::ID, e_csv_column_type::POSITION, e_csv_column_type::VELOCITY }) {
+    }
 };
 #endif
