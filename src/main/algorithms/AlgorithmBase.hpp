11ffb7efe9cbe58deba76ad7da381beb489b0712
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2017-01-06 17:40:43 +0100

    kommentare

--- src/main/algorithms/AlgorithmBase.hpp
+++ src/main/algorithms/AlgorithmBase.hpp
@@ -24,12 +24,38 @@ class AlgorithmBase {
     std::string get_structure_name () {
         return m_stucture_name;
     }
+    /**
+     *
+     * @param p_position_ax current position x
+     * @param p_position_ay current position y
+     * @param p_position_az current position z
+     * @param p_position_bx input=old position x -> output=next position x
+     * @param p_position_by input=old position y -> output=next position y
+     * @param p_position_bz input=old position z -> output=next position z
+     */
     virtual void step_1 (const data_type& p_position_ax,
                          const data_type& p_position_ay,
                          const data_type& p_position_az,
                          data_type&       p_position_bx,
                          data_type&       p_position_by,
                          data_type&       p_position_bz) = 0;
+    /**
+     *
+     * @param p_position_aix current position x for particle i
+     * @param p_position_aiy current position y for particle i
+     * @param p_position_aiz current position z for particle i
+     * @param p_position_bix next position x for particle i
+     * @param p_position_biy next position y for particle i
+     * @param p_position_biz next position z for particle i
+     * @param p_position_ajx current position (array) x for particles j
+     * @param p_position_ajy current position (array) y for particles j
+     * @param p_position_ajz current position (array) z for particles j
+     * @param p_position_bjx next position (array) x for particles j
+     * @param p_position_bjy next position (array) y for particles j
+     * @param p_position_bjz next position (array) z for particles j
+     * @param p_index_j_begin array begin for particles j (inclusive)
+     * @param p_index_j_end array end for particles j (exclusive)
+     */
     virtual void step_2 (const data_type&       p_position_aix,
                          const data_type&       p_position_aiy,
                          const data_type&       p_position_aiz,
