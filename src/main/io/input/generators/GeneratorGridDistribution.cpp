16a21d7e7714f8141d7f81396c5e6eeb82b1995c
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2017-01-19 11:58:31 +0100

    improved particle generator to add a border around all particles, because interactions are calculated around the borders

--- src/main/io/input/generators/GeneratorGridDistribution.cpp
+++ src/main/io/input/generators/GeneratorGridDistribution.cpp
@@ -17,14 +17,15 @@ void GeneratorGridDistribution::initialize_datastructure () {
     Vec3f particle_free_Border_size = m_options.m_bounds * 0.001;
     Benchmark::begin ("ParticleGeneratorGridDistribution");
     data_type     temp  = pow (m_options.m_particle_count, 1.0f / 3.0f);
-    Vec3f         delta = (m_options.m_bounds - particle_free_Border_size * 2) / (temp - 1.0f);
+    Vec3f         delta = (m_options.m_bounds - particle_free_Border_size * 2) / (temp);
     unsigned int  x, y, z;
     unsigned int  tempInt = temp;
     unsigned long count   = 0;
     for (x = 0; x < tempInt; x++) {
         for (y = 0; y < tempInt; y++) {
             for (z = 0; z < tempInt; z++) {
-                m_datastructure.add_particle (particle_free_Border_size + Vec3f (x, y, z) * delta, Vec3f ());
+                m_datastructure.add_particle (particle_free_Border_size + delta * 0.5 + Vec3f (x, y, z) * delta,
+                                              Vec3f ());
                 count++;
             }
         }
@@ -34,7 +35,7 @@ void GeneratorGridDistribution::initialize_datastructure () {
             for (y = 0; y < tempInt; y++) {
                 for (z = 0; z < tempInt; z++) {
                     if (count++ < m_options.m_particle_count) {
-                        m_datastructure.add_particle (particle_free_Border_size +
+                        m_datastructure.add_particle (particle_free_Border_size + delta * 0.5 +
                                                           Vec3f (temp / 2.0 + x, y, z) * delta,
                                                       Vec3f ());
                     }
