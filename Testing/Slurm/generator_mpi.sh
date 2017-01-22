0b2facfa04aaeae4938af65963377c387abb6a81
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2017-01-22 20:28:37 +0100

    jobscript change

--- Testing/Slurm/generator_mpi.sh
+++ Testing/Slurm/generator_mpi.sh
@@ -9,11 +9,11 @@ srun hostname
 srun ../../particle_simulation.x \
 --algorithm=LENNARD_JONES \
 --data_structure=GRID \
---input=GENERATOR_GRID_DISTRIBUTION --count=64000 \
+--input=GENERATOR_GRID_DISTRIBUTION --count=64 \
 --output=FILE_CSV \
---max_iterations=200 \
+--max_iterations=2000 \
 --write_fequency=1 \
 --cut_off_radius=2.5 \
 --timestep=0.005 \
---bounds=50/50/50 \
+--bounds=5/5/5 \
 --verbose
