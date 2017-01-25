14c71f43dde14e28fb63b8db7c3f91dece0cec53
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2017-01-25 07:09:25 +0100

    slurm

--- Testing/Slurm/print_times.sh
+++ Testing/Slurm/print_times.sh
@@ -21,7 +21,7 @@ var_line_statistics_total_runtime="$var_line_statistics_total_runtime,$statistic
 var_line_statistics_total_datastructure_rebuild_count="$statistics_total_datastructure_rebuild_count"
 done
 done
-echo "${var_radius},${var_bounds},${var_initial_speed}${var_line_statistics_total_datastructure_rebuild_count}${var_line_statistics_total_runtime}"
+echo "${var_radius},${var_bounds},${var_initial_speed},${var_line_statistics_total_datastructure_rebuild_count}${var_line_statistics_total_runtime}"
 done
 fi
 done
