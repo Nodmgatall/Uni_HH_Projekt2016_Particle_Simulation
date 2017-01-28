4b902bfa0d22927d70c8f03db82012bcb82b8e3c
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2017-01-28 13:52:55 +0100

    improved script

--- Testing/Slurm/print_times.sh
+++ Testing/Slurm/print_times.sh
@@ -17,8 +17,17 @@ file_content=$(cat "${var_run_index}/${var_test_name}.out")
 #abborted=$(echo "${file_content}" | grep  "Job step aborted")
 statistics_total_runtime=$(echo "${file_content}" | grep "statistics.total_runtime" | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
 statistics_total_datastructure_rebuild_count=$(echo "${file_content}" | grep "statistics.total_datastructure_rebuild_count" | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
+
+aborted=$(echo "${file_content}" | grep "something went badly wrong")
+
+if [[ $param = *[!\ ]* ]]; then
 var_line_statistics_total_runtime="$var_line_statistics_total_runtime,$statistics_total_runtime"
 var_line_statistics_total_datastructure_rebuild_count="$statistics_total_datastructure_rebuild_count"
+else
+var_line_statistics_total_runtime="$var_line_statistics_total_runtime,999999999"
+fi
+
+
 done
 done
 echo "${var_radius},${var_bounds},${var_initial_speed},${var_line_statistics_total_datastructure_rebuild_count}${var_line_statistics_total_runtime}"
