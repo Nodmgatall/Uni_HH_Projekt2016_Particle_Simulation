f12a5bcd28131023753ca65bd650543048e4f8e1
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2017-01-28 17:58:09 +0100

    improved script to test most relevant cases

--- Testing/Slurm/print_times.sh
+++ Testing/Slurm/print_times.sh
@@ -1,11 +1,12 @@
 echo "radius,bounds,initial_speed,total_rebuild_count,GRID_time_1,GRID_time_2,GRID_time_3,LISTEDGRID_time_1,LISTEDGRID_time_2,LISTEDGRID_time_3"
-for var_radius in 2.5 5.0 20.0;
-do
-for var_bounds in 40 80 120;
-do
-if (( $(echo "(($var_radius < 5.0) || ($var_bounds >= 70))" |bc -l) )); then
-for var_initial_speed in 0 5 10;
-do
+
+add_job(){
+var_trash=$1
+var_radius=$2
+var_bounds=$3
+var_initial_speed=$4
+
+
 var_line_statistics_total_runtime="";
 var_line_statistics_total_datastructure_rebuild_count="";
 for var_run_index in 1 2 3;
@@ -14,7 +15,6 @@ for var_datastructure in "GRID" "LISTEDGRID";
 do
 var_test_name="simulation_${var_datastructure}_${var_radius}_${var_bounds}_${var_initial_speed}"
 file_content=$(cat "${var_run_index}/${var_test_name}.out")
-#abborted=$(echo "${file_content}" | grep  "Job step aborted")
 statistics_total_runtime=$(echo "${file_content}" | grep "statistics.total_runtime" | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
 statistics_total_datastructure_rebuild_count=$(echo "${file_content}" | grep "statistics.total_datastructure_rebuild_count" | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
 
@@ -31,7 +31,15 @@ fi
 done
 done
 echo "${var_radius},${var_bounds},${var_initial_speed},${var_line_statistics_total_datastructure_rebuild_count}${var_line_statistics_total_runtime}"
-done
-fi
-done
-done
+
+}
+
+
+add_job $var_datastructure 2.5 80 0
+add_job $var_datastructure 2.5 120 0
+add_job $var_datastructure 5 80 0
+add_job $var_datastructure 5 120 0
+add_job $var_datastructure 2.5 80 1
+add_job $var_datastructure 2.5 120 1
+add_job $var_datastructure 5 80 1
+add_job $var_datastructure 5 120 1
--- Testing/Slurm/slurm_generator.sh
+++ Testing/Slurm/slurm_generator.sh
@@ -1,18 +1,9 @@
 
-for var_datastructure in "GRID" "LISTEDGRID";
-do
-
-for var_radius in 2.5 5.0 20.0;
-do
-
-for var_bounds in 40 80 120;
-do
-
-for var_initial_speed in 0 5 10;
-do
-
-if (( $(echo "(($var_radius < 5.0) || ($var_bounds >= 70))" |bc -l) )); then
-
+add_job(){
+var_datastructure=$1
+var_radius=$2
+var_bounds=$3
+var_initial_speed=$4
 
 var_test_name="simulation_${var_datastructure}_${var_radius}_${var_bounds}_${var_initial_speed}"
 cat > "job_script_${var_test_name}.sh" << EOF
@@ -28,7 +19,7 @@ rm -rf out_64000_particles
 srun hostname
 srun ../../../particle_simulation.x \
 --algorithm=LENNARD_JONES \
---data_structure=GRID \
+--data_structure=${var_datastructure} \
 --input=GENERATOR_GRID_DISTRIBUTION --count=64000 \
 --output=FILE_CSV --out_file_name="${var_test_name}.data" \
 --max_iterations=2000 \
@@ -40,9 +31,20 @@ srun ../../../particle_simulation.x \
 --verbose
 EOF
 chmod +x "job_script_${var_test_name}.sh"
+}
 
-fi
+for var_datastructure in "GRID" "LISTEDGRID";
+do
+
+add_job $var_datastructure 2.5 80 0
+add_job $var_datastructure 2.5 120 0
+add_job $var_datastructure 5 80 0
+add_job $var_datastructure 5 120 0
+add_job $var_datastructure 2.5 80 1
+add_job $var_datastructure 2.5 120 1
+add_job $var_datastructure 5 80 1
+add_job $var_datastructure 5 120 1
+
 done
-done
-done
-done
+
+
