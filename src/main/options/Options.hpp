f1e5f94cf9a893ed9c0b9b41291fcca2ce8ce64e
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2017-01-28 14:36:43 +0100

    dereased iterations until rebuild

--- src/main/options/Options.hpp
+++ src/main/options/Options.hpp
@@ -84,7 +84,7 @@ struct s_options {
     s_options ()
     : m_algorithm_type (e_algorithm_type::LENNARD_JONES), m_autotuneing (false), m_bounds (Vec3f (5.0f, 5.0f, 5.0f)), m_cut_off_radius (0.01),
       m_data_structure_type (e_datastructure_type::GRID), m_input_type (e_input_type::GENERATOR_GRID_DISTRIBUTION), m_in_file_name (""), m_max_iterations (0), m_initial_speed (0),
-      m_max_iterations_between_datastructure_rebuild (100), m_output_type (e_output_type::FILE_CSV), m_out_file_name (""), m_particle_count (0), m_seed (123456789), m_timestep (1),
+      m_max_iterations_between_datastructure_rebuild (50), m_output_type (e_output_type::FILE_CSV), m_out_file_name (""), m_particle_count (0), m_seed (123456789), m_timestep (1),
       m_verbose (false), m_write_fequency (1), m_write_modes ({ e_csv_column_type::ID, e_csv_column_type::POSITION, e_csv_column_type::VELOCITY }) {
         time_t     current_time;
         struct tm* time_info;
