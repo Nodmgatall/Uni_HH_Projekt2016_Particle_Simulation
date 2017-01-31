a411aa8151a4c86e9253319375f8e1540c9e1f45
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2017-01-31 09:13:14 +0100

    changed virtual inheritance

--- src/main/datastructures/DatastructureListGrid.hpp
+++ src/main/datastructures/DatastructureListGrid.hpp
@@ -8,7 +8,7 @@
 #define SRC_MAIN_DATASTRUCTURES_DATASTRUCTURELISTGRID_HPP_
 #include <datastructures/DatastructureGrid.hpp>
 #include <datastructures/DatastructureListBenjamin.hpp>
-class DatastructureListGrid : virtual public DatastructureGrid {
+class DatastructureListGrid : public DatastructureGrid {
   protected:
     void grid_step_2a_calculate_inside_cell (ParticleGroup& p_cell) override;
     void grid_step_2b_calculate_between_cells (ParticleGroup& p_cell_i, ParticleGroup& p_cell_j) override;
@@ -22,6 +22,6 @@ class DatastructureListGrid : virtual public DatastructureGrid {
     /**
      * destructor
      */
-    ~DatastructureListGrid ();
+    virtual ~DatastructureListGrid ();
 };
 #endif /* SRC_MAIN_DATASTRUCTURES_DATASTRUCTURELISTGRID_HPP_ */
