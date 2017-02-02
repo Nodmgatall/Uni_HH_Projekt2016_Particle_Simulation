9631ba4e3e7ab70b3ea80b95d343f2ba5cb616c8
Benjamin Warnke <benjamin@dr-ing-warnke.de>
2017-02-02 08:59:45 +0100

    added openmp-speed-up-test-scripts

--- Testing/Slurm/print_times_openmp_skalierung.sh
+++ Testing/Slurm/print_times_openmp_skalierung.sh
@@ -1,4 +1,4 @@
-echo "radius,bounds,initial_speed,extra_radius,GRID_rebuild_count_1,GRID_rebuild_count_2,GRID_rebuild_count_3,GRID_LIST_rebuild_count_1,GRID_LIST_rebuild_count_2,GRID_LIST_rebuild_count_3,GRID_time_1,GRID_time_2,GRID_time_3,GRID_LIST_time_1,GRID_LIST_time_2,GRID_LIST_time_3"
+echo "radius,bounds,initial_speed,extra_radius,threads,GRID_rebuild_count_1,GRID_rebuild_count_2,GRID_rebuild_count_3,GRID_LIST_rebuild_count_1,GRID_LIST_rebuild_count_2,GRID_LIST_rebuild_count_3,GRID_time_1,GRID_time_2,GRID_time_3,GRID_LIST_time_1,GRID_LIST_time_2,GRID_LIST_time_3"
 
 add_job(){
 var_trash=$1
@@ -6,6 +6,7 @@ var_radius=$2
 var_bounds=$3
 var_initial_speed=$4
 var_radius_extra=$5
+var_threads=$6
 
 var_line_statistics_total_runtime="";
 var_line_statistics_total_datastructure_rebuild_count="";
@@ -13,7 +14,7 @@ for var_datastructure in "GRID" "GRID_LIST";
 do
 for var_run_index in 1 2 3;
 do
-var_test_name="simulation_${var_datastructure}_${var_radius}_${var_bounds}_${var_initial_speed}_${var_radius_extra}"
+var_test_name="simulation_${var_datastructure}_${var_radius}_${var_bounds}_${var_initial_speed}_${var_radius_extra}_${var_threads}"
 file_content=$(cat "${var_run_index}/${var_test_name}.out")
 statistics_total_runtime=$(echo "${file_content}" | grep "statistics.total_runtime" | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
 statistics_total_datastructure_rebuild_count=$(echo "${file_content}" | grep "statistics.total_datastructure_rebuild_count" | grep -Eo '[+-]?[0-9]+([.][0-9]+)?')
@@ -31,26 +32,17 @@ fi
 
 done
 done
-echo "${var_radius},${var_bounds},${var_initial_speed},${var_radius_extra}${var_line_statistics_total_datastructure_rebuild_count}${var_line_statistics_total_runtime}"
+echo "${var_radius},${var_bounds},${var_initial_speed},${var_radius_extra},${var_threads}${var_line_statistics_total_datastructure_rebuild_count}${var_line_statistics_total_runtime}"
 
 }
 
 var_datastructure="GRID"
 
-add_job $var_datastructure 6 60 0 1.2
-add_job $var_datastructure 8 80 0 1.2
-add_job $var_datastructure 12 120 0 1.2
-add_job $var_datastructure 24 240 0 1.2
-add_job $var_datastructure 6 60 1 1.2
-add_job $var_datastructure 8 80 1 1.2
-add_job $var_datastructure 12 120 1 1.2
-add_job $var_datastructure 24 240 1 1.2
-add_job $var_datastructure 6 60 0 1
-add_job $var_datastructure 8 80 0 1
-add_job $var_datastructure 12 120 0 1
-add_job $var_datastructure 24 240 0 1
-add_job $var_datastructure 6 60 1 1
-add_job $var_datastructure 8 80 1 1
-add_job $var_datastructure 12 120 1 1
-add_job $var_datastructure 24 240 1 1
+for var_threads in 1 2 4 8 12 16 20 24
+do
+
+add_job $var_datastructure 8 80 0 1.2 $var_threads
+add_job $var_datastructure 8 80 0 1 $var_threads
+
+done
 
--- Testing/Slurm/slurm_generator_openmp_skalierung.sh
+++ Testing/Slurm/slurm_generator_openmp_skalierung.sh
@@ -5,15 +5,16 @@ var_radius=$2
 var_bounds=$3
 var_initial_speed=$4
 var_radius_extra=$5
+var_threads=$6
 
-var_test_name="simulation_${var_datastructure}_${var_radius}_${var_bounds}_${var_initial_speed}_${var_radius_extra}"
+var_test_name="simulation_${var_datastructure}_${var_radius}_${var_bounds}_${var_initial_speed}_${var_radius_extra}_${var_threads}"
 cat > "job_script_${var_test_name}.sh" << EOF
 #!/bin/bash
 #SBATCH --time=01:00:00
 #SBATCH -J projekt-${var_test_name}
 #SBATCH -o ${var_test_name}.out
 #SBATCH -p west
-#SBATCH -c 24
+#SBATCH -c ${var_threads}
 #SBATCH -N 1-1
 #SBATCH -n 1
 rm -rf out_64000_particles
@@ -37,29 +38,11 @@ chmod +x "job_script_${var_test_name}.sh"
 
 for var_datastructure in "GRID" "GRID_LIST";
 do
+for var_threads in 1 2 4 8 12 16 20 24
+do
 
-add_job $var_datastructure 6 60 0 1.2
-add_job $var_datastructure 8 80 0 1.2
-add_job $var_datastructure 12 120 0 1.2
-add_job $var_datastructure 24 240 0 1.2
-add_job $var_datastructure 6 60 1 1.2
-add_job $var_datastructure 8 80 1 1.2
-add_job $var_datastructure 12 120 1 1.2
-add_job $var_datastructure 24 240 1 1.2
-add_job $var_datastructure 6 60 0 1
-add_job $var_datastructure 8 80 0 1
-add_job $var_datastructure 12 120 0 1
-add_job $var_datastructure 24 240 0 1
-add_job $var_datastructure 6 60 1 1
-add_job $var_datastructure 8 80 1 1
-add_job $var_datastructure 12 120 1 1
-add_job $var_datastructure 24 240 1 1
+add_job $var_datastructure 8 80 0 1.2 $var_threads
+add_job $var_datastructure 8 80 0 1 $var_threads
 
 done
-
-rm -rf 1 2 3
-mkdir 1 2 3
-cp ./*.sh 1
-cp ./*.sh 2
-cp ./*.sh 3
-
+done
