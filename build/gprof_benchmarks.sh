0ddd50bac3192f01a0d3bbf98c3159e713c58008
NodmGatall <oliverheidmann@hotmail.de>
2017-02-20 18:29:14 +0100

    removed old results, changed gprof scripts for more conclusive data

--- build/gprof_benchmarks.sh
+++ build/gprof_benchmarks.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-./slurm_gprof_profiling.sh -s -t 120 -o run_gprof_opti.sh -r 1 -R opti_results particle_sim_opti.x "--count 64000 --data_structure=LINKED_CELLS -m 2000 -r 6.0 -b 60/60/60 --cut_off_extra_factor=1.2 --threads=1 --output=FILE_CSV -t 0.0005 --input=GENERATOR_GRID_DISTRIBUTION"
+./slurm_gprof_profiling.sh -s -t 120 -o run_gprof_opti_64k.sh -r 1 -R opti_64k_results particle_sim_opti.x "--count 59319 --data_structure=LINKED_CELLS -m 500 -r 6.0 -b 60/60/60 --cut_off_extra_factor=1.2 --threads=1 --output=FILE_CSV -t 0.000125 --input=GENERATOR_GRID_DISTRIBUTION"
 
-./slurm_gprof_profiling.sh -s -t 120 -o run_gprof_normal.sh -r 1 -R normal_results particle_simulation.x "--count 64000 --data_structure=LINKED_CELLS -m 2000 -r 6.0 -b 60/60/60 --cut_off_extra_factor=1.2 --threads=1 --output=FILE_CSV -t 0.0005 --input=GENERATOR_GRID_DISTRIBUTION"
+./slurm_gprof_profiling.sh -s -t 120 -o run_gprof_normal_64k.sh -r 1 -R normal_64k_results particle_simulation.x "--count 59319 --data_structure=LINKED_CELLS -m 500 -r 6.0 -b 60/60/60 --cut_off_extra_factor=1.2 --threads=1 --output=FILE_CSV -t 0.000125 --input=GENERATOR_GRID_DISTRIBUTION"
+
+
+./slurm_gprof_profiling.sh -s -t 120 -o run_gprof_opti_32k.sh -r 1 -R opti_32k_results particle_sim_opti.x "--count 32768 --data_structure=LINKED_CELLS -m 500 -r 6.0 -b 60/60/60 --cut_off_extra_factor=1.2 --threads=1 --output=FILE_CSV -t 0.000125 --input=GENERATOR_GRID_DISTRIBUTION"
+
+./slurm_gprof_profiling.sh -s -t 120 -o run_gprof_normal_32k.sh -r 1 -R normal_32k_results particle_simulation.x "--count 32768 --data_structure=LINKED_CELLS -m 500 -r 6.0 -b 60/60/60 --cut_off_extra_factor=1.2 --threads=1 --output=FILE_CSV -t 0.000125 --input=GENERATOR_GRID_DISTRIBUTION"
+
+
+./slurm_gprof_profiling.sh -s -t 120 -o run_gprof_opti_48k.sh -r 1 -R opti_48k_results particle_sim_opti.x "--count 42875 --data_structure=LINKED_CELLS -m 500 -r 6.0 -b 60/60/60 --cut_off_extra_factor=1.2 --threads=1 --output=FILE_CSV -t 0.000125 --input=GENERATOR_GRID_DISTRIBUTION"
+
+./slurm_gprof_profiling.sh -s -t 120 -o run_gprof_normal_48k.sh -r 1 -R normal_48k_results particle_simulation.x "--count 42875 --data_structure=LINKED_CELLS -m 500 -r 6.0 -b 60/60/60 --cut_off_extra_factor=1.2 --threads=1 --output=FILE_CSV -t 0.000125 --input=GENERATOR_GRID_DISTRIBUTION"
 
